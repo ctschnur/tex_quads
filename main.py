0a168b29667223e1524f7e22f8a279a089a4f3d0
Your Name <you@example.com>
2018-05-15 18:36:07 +0200

    Quad is now also colored

--- main.py
+++ main.py
@@ -8,56 +8,56 @@ from panda3d.core import Geom
 from panda3d.core import GeomVertexWriter
 from panda3d.core import GeomTriangles 
 from panda3d.core import GeomNode
+
 class MyApp(ShowBase):
  
     def __init__(self):
         ShowBase.__init__(self)
 
-        # # teapot
-        # self.scene = self.loader.loadModel("models/teapot")
-        # self.scene.reparentTo(self.render)
         self.disableMouse()
         self.camera.setPos(0, -5, 0)
 
         # Own Geometry
 
-        #in this example, we'll show how to make a square from scratch 
-        #There is no "GeomQuad" class so we have to use two triangles. 
-
-        #step 1) create GeomVertexData and add vertex information 
-        format = GeomVertexFormat.getV3() 
-        vdata = GeomVertexData("vertices", format, Geom.UHStatic) 
+        format = GeomVertexFormat.getV3c4()
+        vdata = GeomVertexData("colored_quad", format, Geom.UHStatic) 
         vdata.setNumRows(4) 
 
-        vertexWriter = GeomVertexWriter(vdata, "vertex") 
-        vertexWriter.addData3f(0,0,0) 
-        vertexWriter.addData3f(1,0,0) 
-        vertexWriter.addData3f(1,0,1) 
-        vertexWriter.addData3f(0,0,1) 
+        vertexPosWriter = GeomVertexWriter(vdata, "vertex") 
+        vertexPosWriter.addData3f(0,0,0) 
+        vertexPosWriter.addData3f(1,0,0) 
+        vertexPosWriter.addData3f(1,0,1) 
+        vertexPosWriter.addData3f(0,0,1) 
 
-        #step 2) make primitives and assign vertices to them 
-        tris=GeomTriangles(Geom.UHStatic) 
+        # let's also add color to each vertex
+        colorWriter = GeomVertexWriter(vdata, "color") 
+        colorWriter.addData4f(0,0,1,1) 
+        colorWriter.addData4f(0,0,1,1) 
+        colorWriter.addData4f(0,0,1,1) 
+        colorWriter.addData4f(0,0,1,1) 
 
-        #have to add vertices one by one since they are not in order 
-        tris.addVertex(0) 
-        tris.addVertex(1) 
-        tris.addVertex(3) 
+        # make primitives and assign vertices to them (primitives and primitive
+        # groups can be made independently from vdata, and are later assigned 
+        # to vdata)
+        tris = GeomTriangles(Geom.UHStatic) 
 
-        #indicates that we have finished adding vertices for the first triangle. 
+        # 1st triangle
+        tris.addVertices(0, 1, 3) 
+        tris.closePrimitive()  # the 1st primitive is finished
+
+        # 2nd triangle
+        tris.addVertices(1,2,3)
         tris.closePrimitive() 
 
-        #since the coordinates are in order we can use this convenience function. 
-        tris.addConsecutiveVertices(1,3) #add vertex 1, 2 and 3 
-        tris.closePrimitive() 
+        # make a Geom object to hold the primitives 
+        quadGeom = Geom(vdata) 
+        quadGeom.addPrimitive(tris) 
 
-        #step 3) make a Geom object to hold the primitives 
-        squareGeom=Geom(vdata) 
-        squareGeom.addPrimitive(tris) 
-
-        #now put squareGeom in a GeomNode. You can now position your geometry in the scene graph. 
-        squareGN=GeomNode("square") 
-        squareGN.addGeom(squareGeom) 
-        render.attachNewNode(squareGN)
+        # now put quadGeom in a GeomNode. You can now position your geometry 
+        # in the scene graph. 
+        quadGN = GeomNode("quad") 
+        quadGN.addGeom(quadGeom) 
+        render.attachNewNode(quadGN)
 
 app = MyApp()
 app.run()
