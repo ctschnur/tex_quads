2d5e9b7a9f1ba70a4a144e61e784a36140b87ffd
Your Name <you@example.com>
2018-05-23 19:21:15 +0200

    bit of refactoring; the translation transform operates different for differently sized geometries; ---> please use setPos directly through panda3d and remove all the matrix variables in the wrapper

--- latexObject.py
+++ latexObject.py
@@ -11,167 +11,130 @@ from direct.interval.LerpInterval import *
 
 class Animator:   # only derivatives of this class should be called
     def __init__(self):
-        self.renderit = True
-        self.customTransform = Mat4()
-        self.net_trafo_at_t0_mat4 = Mat4()
-        self.standardTransform = Mat4()  # each subclass has a specific transform (Line, LatexObject, ...)
+        self.net_custom_initial_static_trafo = Mat4()
 
-    def initiateTranslationMovement(self, v_x=0., v_z=0., delta_t=0., delay=0.): 
-        extraArgs = [ 
-                v_x,
-                v_z,
-                delta_t
-            ]
-        self.interval = LerpFunc(self.translate_update_matrix, duration=1.0, blendType="easeInOut", extraArgs=extraArgs) 
-        Sequence(Wait(delay), self.interval).start()
+        # when applying a transformation, this is the matrix made of 
+        # previous transformations due to animations 
+        # where the new one is continually applied to 
+        self.net_custom_anim_trafo = Mat4()  
+        self.last_custom_anim_mat4 = Mat4()
 
-    def translate_update_matrix(self, t, v_x, v_z, delta_t):
-        self.customTransform = (Mat4.translateMat(v_x*(t/delta_t), 0., v_z*(t/delta_t)) * 
-                                self.net_trafo_at_t0_mat4)
-        self._apply_net_trafo_to_nodepath()
+        self.net_custom_matrix = Mat4()
 
-    def _apply_net_trafo_to_nodepath(self):
-        self.quad_nodepath.setMat(self.customTransform * self.standardTransform)
+    def makeObject(self):
+        """creates geometry and doesn't transform it"""
+        self.quad_node = customGeometry.createTexturedUnitQuadGeomNode()
+        self.quad_nodepath = render.attachNewNode(self.quad_node)
+        self.applyImageAndTexture()
 
-    def render(self): 
-        # after each detaching from the scenegraph, if you want to attach the
-        # node again, you have to redo all the things that can only be operated on a
-        # the corresponding nodepath. A nodepath that has a texture and 
-        # transformation applied to it can't be detached and reattached to 
-        # the render scenegraph while keeping the applied texture and 
-        # transformation information 
-        # That's why we only hide() the nodepath, to make it invisible to all
-        # cameras and show() it if necessary
-        self.renderit = True
-        self.quad_nodepath.show()
+    def updateCurrentTranslationMat(self, t, duration, v_x, v_z):
+        self.net_custom_anim_trafo = (
+            Mat4.translateMat(v_x*(t/duration), 0., v_z*(t/duration)) * 
+            self.last_custom_anim_mat4 )
 
-    def hide(self):
-        self.renderit = False
-        self.quad_nodepath.hide()
+        self.updateNetMatrix()
 
+    def updateNetMatrix(self):
+        self.net_custom_matrix = self.last_custom_anim_mat4 * self.net_custom_anim_trafo * self.net_custom_initial_static_trafo
+        self.quad_nodepath.setMat(self.net_custom_matrix)
+
+    # --- animations
+    def initiateTranslationMovement(self, v_x=0., v_z=0., duration=0., delay=0.): 
+        extraArgs = [duration, v_x, v_z]
+        self.p3d_interval = LerpFunc(self.updateCurrentTranslationMat, duration=duration, extraArgs=extraArgs) 
+        Sequence(Wait(delay), self.p3d_interval).start()
 
 class LatexObject(Animator): 
     def __init__(self, tex_expression):
         Animator.__init__(self)
 
         self.tex_expression = tex_expression
-        self.makeObject()
+        self.makeObject() 
 
-    def getImageAndApplyTexture(self):
-        # TODO: check if latex image is compiled and loaded, and if so retrieve it and not
-        # load it anew
-        # get expression hash
-        expr_hash = hashlib.sha256(str(self.tex_expression).encode("utf-8")).hexdigest()
-        # load image with that hash
-        from latexExpressionManager import LatexImageManager, LatexImage
-        myLatexImage = LatexImageManager.retrieveLatexImageFromHash(expr_hash)
-        if myLatexImage is None:  # it wasn't compiled yet, so do it
-            myLatexImage = LatexImage(expression_str=self.tex_expression)
-            myLatexImage.compileToPNG()
-            LatexImageManager.addLatexImageToCompiledSet(myLatexImage)
-            LatexImageManager.addLatexImageToLoadedSet(myLatexImage)
+        # all the initial transformations
+        # TODO: this should be a setter function
+        self.net_custom_initial_static_trafo = self.trafo_UnitSquad_To_ImageDisplay()
 
-        self.myPNMImage = myLatexImage.getPNMImage()
+        self.updateNetMatrix()
+
+    def trafo_UnitSquad_To_ImageDisplay(self):
+        """a unit quad with an image in the background is being scaled so that
+        the pixel height and width fits exactly with the screen resolution"""
         
-        # PNMImage() is p3d's own image loading and handling utility class
-        # self.myPNMImage = textureUtils.getImageFromFile()  # default: sample.png
-        # Texture() is p3d's own texture class that interacts well with the
-        # PNMImage() class
-        self.myTexture = textureUtils.getTextureFromImage(self.myPNMImage)
+        def getMat4_scaleUnitQuadProperly(image_width_pixels, image_height_pixels):
+            return (
+                getMat4_scale_quad_for_texture_pixels_to_match_screen_resolution() * 
+                getMat4_scale_unit_quad_to_image_aspect_ratio(image_width_pixels, image_height_pixels))
 
-        # assign the Texture() to the NodePath() that contains the Geom()
-        # only on the nodepath you can assign textures with setTexture()
-        # priority 1 could also be 0 here, since nothing is inherited
-        self.quad_nodepath.setTexture(self.myTexture, 1)
-        self.quad_nodepath.setTransparency(TransparencyAttrib.MAlpha)
+        return getMat4_scaleUnitQuadProperly(
+            self.myPNMImage.getXSize(), self.myPNMImage.getYSize())
 
     def makeObject(self):
-        
+        """creates geometry and doesn't transform it"""
         self.quad_node = customGeometry.createTexturedUnitQuadGeomNode()
-        # the concept of a NodePath() only makes sense only the node is attached 
-        # to a scenegraph, so do it:
         self.quad_nodepath = render.attachNewNode(self.quad_node)
-        # specifically for this type of data:
-        self.getImageAndApplyTexture()
 
-        # Whatever is your texture, it now has been applied to a unit square.
-        # If all you want to do is show an image, it is probably stretched/squished
-        # into the unit square.
-        # There are two solutions to this problem: 
-        # 1. You can scale the unit square by applying a custom model matrix
-        #    onto your unit square geometry. p3d provides utility functions for
-        #    that (e.g. nodepath.setSx/y/z(float)), but I prefer to construct 
-        #    my own matrix and then give it to p3d
-        # 2. You could apply a transform on your textures (u,v) coordinates
+        def applyImageAndTexture():
+            """assign the Texture() to the NodePath() that contains the Geom()
+            load image with the object's hash"""
+            expr_hash = hashlib.sha256(str(self.tex_expression).encode("utf-8")).hexdigest()
+            from latexExpressionManager import LatexImageManager, LatexImage
+            myLatexImage = LatexImageManager.retrieveLatexImageFromHash(expr_hash)
+            if myLatexImage is None:
+                myLatexImage = LatexImage(expression_str=self.tex_expression)
+                myLatexImage.compileToPNG()
+                LatexImageManager.addLatexImageToCompiledSet(myLatexImage)
+                LatexImageManager.addLatexImageToLoadedSet(myLatexImage)
 
-        self.standardTransform = LatexObject.getMat4_scaleUnitQuadProperly(self.myPNMImage.getXSize(), self.myPNMImage.getYSize())
+            self.myPNMImage = myLatexImage.getPNMImage() 
+            self.myTexture = textureUtils.getTextureFromImage(self.myPNMImage)
+            self.quad_nodepath.setTexture(self.myTexture, 1)
+            self.quad_nodepath.setTransparency(TransparencyAttrib.MAlpha)
 
-        self._apply_net_trafo_to_nodepath()
+        applyImageAndTexture()
 
-    @staticmethod
-    def getMat4_scaleUnitQuadProperly(image_width_pixels, image_height_pixels):
-        return (
-            getMat4_scale_quad_for_texture_pixels_to_match_screen_resolution() * 
-            getMat4_scale_unit_quad_to_image_aspect_ratio(image_width_pixels, image_height_pixels))
 
 class Line(Animator): 
     thickness = 0.02
     length = 1.
-    position = Vec3(0., 0., 0.) 
 
     def __init__(self):
         Animator.__init__(self)
 
-        self.makeObject()
- 
+        self.makeObject() 
+
+        # initial static transformation
+        def getMat4_scaleUnitQuadForLine(thickness, length):
+            quad_scalez = float(thickness)
+            return Mat4(1., 0, 0, 0, 
+                        0, 1., 0, 0, 
+                        0, 0, quad_scalez * 1., 0, 
+                        0, 0, 0, 1) 
+
+        # TODO: this should be a setter function
+        self.net_custom_initial_static_trafo = getMat4_scaleUnitQuadForLine(self.thickness, self.length)
+
     def makeObject(self): 
         self.quad_node = customGeometry.createColoredUnitQuadGeomNode(color_vec4=Vec4(1., 1., 1., 1.))
-        # the concept of a NodePath() makes sense only if the node is attached 
-        # to a scenegraph, so do it:
         self.quad_nodepath = render.attachNewNode(self.quad_node)
 
-        self.standardTransform = Line.getMat4_scaleUnitQuadForLine(self.thickness, self.length, self.position)
 
-        self._apply_net_trafo_to_nodepath()
-
-    @staticmethod
-    def getMat4_scaleUnitQuadForLine(thickness, length, position):
-        # scale height down
-        # quad_scalex = float(length)
-        quad_scalez = float(thickness)
-        return Mat4(1., 0, 0, 0, 
-                    0, 1., 0, 0, 
-                    0, 0, quad_scalez * 1., 0, 
-                    0, 0, 0, 1) 
-
-
-class ArrowForLine(Animator): 
-    eqilateral_length = Line.thickness * 4.
-    position = Vec3(0., 0., 0.) 
+class ArrowHead(Animator): 
+    equilateral_length = Line.thickness * 4.
 
     def __init__(self):
         Animator.__init__(self)
 
-        self.makeObject()
- 
+        self.makeObject() 
+        
+        # initial static transformation
+        def getMat4_scaleUnitQuad(thickness, length):
+            scale = 0.1
+            return Mat4.scaleMat(scale, 0., scale)
+
+        # TODO: this should be a setter function
+        self.net_custom_initial_static_trafo = getMat4_scaleUnitQuad(Line.thickness, Line.length)
+
     def makeObject(self): 
         self.quad_node = customGeometry.createColoredArrowGeomNode(color_vec4=Vec4(1., 1., 1., 1.))
-        # the concept of a NodePath() makes sense only if the node is attached 
-        # to a scenegraph, so do it:
         self.quad_nodepath = render.attachNewNode(self.quad_node)
-
-        self.standardTransform = ArrowForLine.getMat4_scaleGeometry(self.eqilateral_length, self.position)
-
-        self._apply_net_trafo_to_nodepath()
-
-    @staticmethod
-    def getMat4_scaleGeometry(equilateral_length, position):
-        # scale height down
-        # quad_scalex = float(length)
-        scale = float(equilateral_length)
-        return Mat4(scale, 0, 0, 0, 
-                    0, 1., 0, 0, 
-                    0, 0, scale* 1., 0, 
-                    0, 0, 0, 1) 
-
-
--- main.py
+++ main.py
@@ -18,16 +18,15 @@ class MyApp(ShowBase):
         # create some LatexObjects
 
         myLatexObject = LatexObject("Obj 1")
-        myLatexObject.initiateTranslationMovement(v_x=-1., delta_t=1.0, delay=0.5)
+        myLatexObject.initiateTranslationMovement(v_x=1., v_z=1., duration=1., delay=0.5)
         myLatexObject2 = LatexObject(r"$a \cdot b  - \frac{c}{d}$")
-        myLatexObject2.initiateTranslationMovement(v_x=1., delta_t=1.0)
+        myLatexObject2.initiateTranslationMovement(v_x=1., v_z=1., duration=1.5, delay=0.)
 
-        
         numberLine = Line()
-        numberLine.initiateTranslationMovement(v_x=.5, v_z=.3, delta_t=3., delay=0.)
-        
-        Arrow = ArrowForLine()
-        # Arrow.initiateTranslationMovement(v_x=.5, v_z=.3, delta_t=3., delay=0.)
+        numberLine.initiateTranslationMovement(v_x=1., v_z=0., duration=1., delay=0.)
+
+        # Arrow = ArrowHead()
+        # Arrow.initiateTranslationMovement(v_x=1., v_z=0., duration=1., delay=0.)
         
         # childs = render.getChildren()
         # for c_child in childs: 
