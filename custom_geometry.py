3fcfc968a13e6abc8849d4078e73a26253d0fc33
Your Name <you@example.com>
2018-08-22 12:24:01 +0200

    triangulation algorithm spits out wrong indices indexing the original point cloud

--- custom_geometry.py
+++ custom_geometry.py
@@ -154,47 +154,56 @@ def createColoredArrowGeomNode(color_vec4=Vec4(0., 0., 1., 1.)):
 
 
 def create_colored_polygon2d_GeomNode_from_point_cloud(point_cloud, color_vec4=Vec4(0., 0., 1., 1.)):
-    # Own Geometry
-    _, idx = np.unique(point_cloud, return_index=True, axis=0) 
-    point_cloud = np.array(point_cloud)[np.sort(idx)]  # that goes into the geomNode
 
-    # triangulate
-    triangles, indices = tripy_modified.earclip([tuple(elem) for elem in point_cloud])
-    indices = np.array(indices)  # that goes into the triangles
-    
+    # for triangulation (with tripy) the elements of point_cloud
+    # must be 3-tuples instead of np.arrays
+    triangles, indices = tripy_modified.earclip(
+        [tuple(elem) for elem in point_cloud])
+    # it returns 3-tuples, too. 
+    # it returns also -1 as an index, if it means the last element of the point
+    # cloud
+
+    print(point_cloud)
+    print([tuple(elem) for elem in point_cloud])
+    print(triangles)
+    indices = np.array(indices)  # convert tuples back to arrays
+    print(indices)
+    indices[indices == -1] = len(point_cloud) - 1
+    print(indices)
 
     # format = GeomVertexFormat.getV3c4t2()
     format = GeomVertexFormat.getV3c4()
     vdata = GeomVertexData("colored_polygon", format, Geom.UHStatic)
     vdata.setNumRows(4)
-    
-    # let's also add color to each vertex
-    colorWriter = GeomVertexWriter(vdata, "color")
-    vertexPosWriter = GeomVertexWriter(vdata, "vertex")
+    # make a Geom object to hold the primitives
+    geom = Geom(vdata)  # give it a reference to the 
+    # add position and color to each vertex
+    vertex_pos_writer = GeomVertexWriter(vdata, "vertex")
+    vertex_color_writer = GeomVertexWriter(vdata, "color")
 
+    # fill vertex buffer
     for point in point_cloud: 
-        vertexPosWriter.addData3f(point[0], 0, point[1])
-        colorWriter.addData4f(color_vec4)
+        vertex_pos_writer.addData3f(point[0], 0, point[1])  # z is up
+        vertex_color_writer.addData4f(color_vec4)
 
-    # make primitives and assign vertices to them (primitives and primitive
-    # groups can be made independently from vdata, and are later assigned
-    # to vdata)
-    
-    tris = GeomTriangles(Geom.UHStatic)
+    # create the GeomPrimitive (just one) by filling the index buffer 
+    # in stages. The documentation says 'Each GeomPrimitive object actually
+    # stores several different individual primitives, each of which is
+    # represwended simply as a list of vertex numbers, indexing into the
+    # vertices stored in the associated GeomVertexData'
+
+    tris = GeomTriangles(Geom.UHStatic)  # derived from GeomPrimitive
     for idx_triple in indices: 
         tris.addVertex(int(idx_triple[0]))
         tris.addVertex(int(idx_triple[1]))
         tris.addVertex(int(idx_triple[2]))
+        # close the current primitive (not the GeomPrimitive!)
         tris.closePrimitive()
 
-    # make a Geom object to hold the primitives
-    polygonGeom = Geom(vdata)
-    polygonGeom.addPrimitive(tris)
+    geom.addPrimitive(tris)
 
-    # now put quadGeom in a GeomNode. You can now position your geometry
-    # in the scene graph.
-    polygonGN = GeomNode("colored_polygon_node")
-    polygonGN.addGeom(polygonGeom)
+    geom_node = GeomNode("colored_polygon_node")
+    geom_node.addGeom(geom)
 
-    return polygonGN
+    return geom_node 
 
--- main.py
+++ main.py
@@ -19,18 +19,18 @@ class MyApp(ShowBase):
 
         # create some LatexObjects
 
-        myLatexObject = LatexTextureObject("Obj 1")
-        myLatexObject.initiateTranslationMovement(v_x=1.5, v_z=0., duration=1., delay=0.)
-        myLatexObject2 = LatexTextureObject(r"$a \cdot b  - \frac{c}{d}$")
-        myLatexObject2.initiateTranslationMovement(v_x=1.5, v_z=0., duration=1., delay=0.)
+        # myLatexObject = LatexTextureObject("Obj 1")
+        # myLatexObject.initiateTranslationMovement(v_x=1.5, v_z=0., duration=1., delay=0.)
+        # myLatexObject2 = LatexTextureObject(r"$a \cdot b  - \frac{c}{d}$")
+        # myLatexObject2.initiateTranslationMovement(v_x=1.5, v_z=0., duration=1., delay=0.)
 
         axis = Axis()
 
-        point_clouds = get_point_clouds_from_svg("tests/svgpathmanipulaton/main.svg")
-        
-        polygon2d2 = Polygon2d(
-            point_clouds[0]/10.)
-        
+        # point_clouds = get_point_clouds_from_svg("tests/svgpathmanipulaton/main.svg") 
+        # polygon2d2 = Polygon2d(np.array(point_clouds[0])/10.)
+        cloud = np.array([(0,1), (-1, 0), (0, -1), (1, 0)])
+        polygon2d2 = Polygon2d(cloud)
+
         childs = render.getChildren()
         print(len(childs))
 
