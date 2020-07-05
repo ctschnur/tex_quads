256a5c7a389125f82f341cbc5d1a6e2d26f925c0
534ttl3 <534ttl3@gmail.com>
2020-07-06 01:35:07 +0200

    dragging onMove Task started, but not yet working

--- main.py
+++ main.py
@@ -29,62 +29,101 @@ from plot_utils.bezier_curve import BezierCurve
 from panda3d.core import CollisionTraverser, CollisionHandlerQueue, CollisionRay, CollisionNode, GeomNode, BitMask32, VBase4
 
 
+
 class PickableObjectManager:
     """ Each pickable object has to have an individual tag,
     at creation, use this class to generate a new individual tag for it """
-    def __init__( self ):
+
+    def __init__(self):
         self.objectIdCounter = 0
         # self.objectDict = dict()
 
-    def tag( self, objectNp, # objectClass
-    ):
+    def tag(self, objectNp,  # objectClass
+            ):
         self.objectIdCounter += 1
         objectTag = str(self.objectIdCounter)
         objectNp.setTag('objectId', objectTag)
-    #     self.objectDict[objectTag] = objectClass
+        # self.objectDict[objectTag] = objectClass
 
-    # def get( self, objectTag ):
+    # def get(self, objectTag):
     #     if objectTag in self.objectDict:
-    #       return self.objectDict[objectTag]
+    #         return self.objectDict[objectTag]
+    #     return None
+
+
+class DraggableObjectManager:
+    """ Each pickable object has to have an individual tag,
+    at creation, use this class to generate a new individual tag for it """
+
+    def __init__(self):
+        self.objectIdCounter = 0
+        # self.objectDict = dict()
+
+    def tag(self, objectNp, drag_drop_object):
+        self.objectIdCounter += 1
+        objectTag = str(self.objectIdCounter)
+        objectNp.setTag('objectId', objectTag)
+        # self.objectDict[objectTag] = drag_drop_object
+
+    # def get(self, objectTag):
+    #     if objectTag in self.objectDict:
+    #         return self.objectDict[objectTag]
     #     return None
 
 
 class PickablePoint(Point):
     """ a flat point (2d box) parented by render """
+
     def __init__(self, pickableObjectManager, **kwargs):
         Point.__init__(self, **kwargs)
 
         self.nodePath.setColor(1., 0., 0., 1.)
         pickableObjectManager.tag(self.nodePath, )
 
-        # self.box.setTag('MyObjectTag', '1')
+    # def drag(self, mouseNp):
+    #     # that should be the only thing
+    #     self.model.wrtReparentTo(mouseNp)
 
+    #     # self.box.setTag('MyObjectTag', '1')
 
 class DragDropObject:
-    dragMask = BitMask32.bit(1)
-    dropMask = BitMask32.bit(1)
-    highlight = VBase4(.3,.3,.3,1)
+    dragMask = (  # BitMask32.bit(1)
+        GeomNode.getDefaultCollideMask()
+    )
+    # dropMask = BitMask32.bit(1)
+    # highlight = VBase4(.3,.3,.3,1)
 
-    def __init__( self, np, objectManager):
+    def __init__(self, np, draggable_object_manager, **kwargs):
         self.model = np
         self.previousParent = None
         self.model.setCollideMask(dragMask)
-        self.objectManager = objectManager
-        # import ipdb; ipdb.set_trace()  # noqa BREAKPOINT
-        self.objectManager.tag( self.model, self )
+        self.draggable_object_manager = draggable_object_manager
+        self.draggable_object_manager.tag(self.model, self)
 
-    def onPress( self, mouseNp ):
+    def onPress(self, mouseNp):
         self.previousParent = self.model.getParent()
-        self.model.wrtReparentTo( mouseNp )
+        self.model.wrtReparentTo(mouseNp)
         self.model.setCollideMask(BitMask32.allOff())
 
-    def onRelease( self ):
-        self.model.wrtReparentTo( self.previousParent )
+    def onRelease(self):
+        self.model.wrtReparentTo(self.previousParent)
         self.model.setCollideMask(dragMask)
+
+
+class DraggablePoint(PickablePoint, DragDropObject):
+    """ a flat point (2d box) parented by render """
+
+    def __init__(self, pickableObjectManager, **kwargs):
+        PickablePoint.__init__(self, **kwargs)
+        DragDropObject.__init__(self, **kwargs)
+
+        self.nodePath.setColor(1., 0., 0., 1.)
+        pickableObjectManager.tag(self.nodePath, )
 
 
 class CollisionPicker:
     """ This stores a ray, attached to a camera """
+
     def __init__(self, camera_gear, render, mousewatchernode, base):
 
         # -- things that are needed to do picking from different camera orientations
@@ -96,12 +135,24 @@ class CollisionPicker:
 
         self.mouse_watcher_node = mousewatchernode
 
+
+        # ---- dragging
+        base.accept("escape", sys.exit)
+        base.accept('mouse1', self.onPress)
+        base.accept('mouse1-up', self.onRelease)
+        self.draggedObj = None
+
+        taskMgr.add( self.mouseMoverTask, 'mouseMoverTask' )
+
+        # ----
+
         # -- things that are needed for collision detection by picking a 3d object
         self.pick_traverser = CollisionTraverser()
         self.collision_queue = CollisionHandlerQueue()
 
         # ---- build the CollisionRay
-        self.pick_collision_ray = CollisionRay()  # the pick ray is apparently a 'Solid'
+        # the pick ray is apparently a 'Solid'
+        self.pick_collision_ray = CollisionRay()
 
         # -- a ray is a half-inifinite straight line
         # it is supposed to shoot out orthogonally to the view plane and hit an object
@@ -109,25 +160,30 @@ class CollisionPicker:
 
         # -- TODO: update this every time the orbiter camera position changes
         # first, transform the (0,1,0) vector into render's coordinate system
-        self.pick_collision_ray.setDirection(render.getRelativeVector(camera, Vec3(0, 1, 0)))
+        self.pick_collision_ray.setDirection(
+            render.getRelativeVector(camera, Vec3(0, 1, 0)))
 
         # ---- build the CollisionNode
         self.pick_collision_node = CollisionNode('pick_collision_ray')
-        self.pick_collision_node.addSolid(self.pick_collision_ray)  # the pick ray is actually a 3d object
+        # the pick ray is actually a 3d object
+        self.pick_collision_node.addSolid(self.pick_collision_ray)
 
         # attach the CollisionNode to the camera (not the CollisionRay)
-        self.pick_collision_node_nodePath = self.camera.attachNewNode(self.pick_collision_node)
+        self.pick_collision_node_nodePath = self.camera.attachNewNode(
+            self.pick_collision_node)
 
         # set a collide mask to the pick_collision_node, 2 objects that should be able to collide must have the same collide mask!
         self.pick_collision_node.setFromCollideMask(GeomNode.getDefaultCollideMask()
-            # DragDropObject.dragMask
-        )  # set bit 20 (Default) to the ray
+                                                    # DragDropObject.dragMask
+                                                    )  # set bit 20 (Default) to the ray
 
         # add the ray as sth. that can cause collisions, and tell it to fill up our collision queue object when traversing and detecting
-        self.pick_traverser.addCollider(self.pick_collision_node_nodePath, self.collision_queue)
+        self.pick_traverser.addCollider(
+            self.pick_collision_node_nodePath, self.collision_queue)
 
+        self.pick_collision_node_nodePath.show()
 
-    def onMouseTask(self):
+    def onPress(self):
         if self.mouse_watcher_node.hasMouse() == False:
             return
 
@@ -135,25 +191,53 @@ class CollisionPicker:
         # mouse_pos = base.mouseWatcherNode.getMouse()
         mouse_pos = self.base.mouseWatcherNode.getMouse()
 
-        self.pick_collision_ray.setFromLens(self.camera.node(), mouse_pos[0], mouse_pos[1])
+        self.pick_collision_ray.setFromLens(
+            self.camera.node(), mouse_pos[0], mouse_pos[1])
 
         # now actually (manually) traverse to see if the two objects are collided (traverse the render tree (is the camera included there?))
-        self.pick_traverser.traverse(render)  # this should fill up the collision queue
+        # this should fill up the collision queue
+        self.pick_traverser.traverse(render)
 
         if self.collision_queue.getNumEntries() > 0:
             # first, sort the entries (? which direction does it do that? to the camera?)
-            entry = self.collision_queue.getEntry(0)  # get the first entry (closest I suppose)
-            picked_obj = entry.getIntoNodePath()  # return the nodepath of the 'into-object' of the collision
-            picked_obj_with_tag = picked_obj.findNetTag('objectId')  # get the object specifically with that tag (may be a child of the picked object)
+            # get the first entry (closest I suppose)
+            entry = self.collision_queue.getEntry(0)
+            # return the nodepath of the 'into-object' of the collision
+            picked_obj = entry.getIntoNodePath()
+            # get the object specifically with that tag (may be a child of the picked object)
+            picked_obj_with_tag = picked_obj.findNetTag('objectId')
 
             # check to see if indeed an object was picked, and which posiition it has
             if not picked_obj_with_tag.isEmpty():
                 picked_obj_pos = entry.getSurfacePoint(render)
-                # import ipdb; ipdb.set_trace()  # noqa BREAKPOINT
+
                 print("picked object: ",
                       # picked_obj_with_tag.getTags(), " tag: ",
                       ", the object: ", picked_obj_with_tag, ", the position: ", picked_obj_pos)
                 picked_obj_with_tag.setColor(1., 1., 1., 1.)
+
+                # TODO: attach a
+                # recalculate position of
+                picked_obj_with_tag.wrtReparentTo(self.pick_collision_node_nodePath)
+                picked_obj_with_tag.setCollideMask(BitMask32.allOff())
+
+    def onRelease( self ):
+        print("release")
+        # obj = self.collisionCheck()
+        # self.draggedObj.onRelease() # self.cnp )
+        # if obj is not None:
+        #     self.draggedObj.onCombine( obj )
+
+
+    def mouseMoverTask( self, task ):
+        if base.mouseWatcherNode.hasMouse():
+            mpos = base.mouseWatcherNode.getMouse()
+            # TODO:
+            # project it into the view plane
+            print(mpos)
+            self.pick_collision_node_nodePath.setPos(mpos[0], 0, mpos[1])
+        return task.cont
+
 
 
 class MyApp(ShowBase):
@@ -182,14 +266,13 @@ class MyApp(ShowBase):
 
         BezierCurve(point_coords_arr)
 
-
-
         pickableObjectManager = PickableObjectManager()
 
-        collisionPicker = CollisionPicker(ob, render, self.mouseWatcherNode, base)
+        collisionPicker = CollisionPicker(
+            ob, render, self.mouseWatcherNode, base)
 
-        myDirectObject = DirectObject.DirectObject()
-        myDirectObject.accept('mouse1', collisionPicker.onMouseTask)
+        # myDirectObject = DirectObject.DirectObject()
+        # myDirectObject.accept('mouse1', collisionPicker.onMouseTask)
 
         control_points = []
         for p in point_coords_arr:
@@ -197,8 +280,8 @@ class MyApp(ShowBase):
                                pos=Vec3(*p), thickness=10, point_type="quasi2d")
             pt.nodePath.setHpr(90, 0, 0)  # 90 degrees yaw
             pt.nodePath.showBounds()
-            control_points.append(pt)
 
+            control_points.append(pt)
 
         def findChildrenAndSetRenderModeRecursively(parentnode):
             children = parentnode.get_children()
