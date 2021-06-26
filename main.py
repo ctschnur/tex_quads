e664a7d16086052717011337afbf6fc88658b0f1
534ttl3 <534ttl3@gmail.com>
2021-06-26 16:43:26 +0200

    adding live audio plotting time and freq domain

--- main.py
+++ main.py
@@ -51,7 +51,6 @@ from recording.recorder_for_live_updating import Recorder_for_live_updating
 import scipy
 from scipy import fftpack
 
-
 class F2dUpdater:
     """ """
     def __init__(self, next_color_func, xy_datas, f2d):
@@ -125,19 +124,11 @@ class StreamFramesFromRecorder:
     """ stream the frames from the recorder to a Frame2d """
     def __init__(self, f2d):
         self.f2d = f2d
-        print("1 hey")
         self.recorder = Recorder_for_live_updating()
-        print("2 hey")
         self.recorder.do_record(
-            # self.state.is_recording,
-            # self.state.is_paused,
-            # self.state.is_recording_finished
             lambda: True,
             lambda: False,
             lambda: False)
-        print("3 hey")
-
-        # self.f2d.set_xlims()
 
         self.factor = 1.0e-1 * 0.2
 
@@ -146,14 +137,7 @@ class StreamFramesFromRecorder:
 
         taskMgr.add(self.update_task, 'foo')
 
-
-
     def update_task(self, task):
-        # print("update")
-        # return task.cont
-        print("update 0")
-        print("self.recorder.is_recorder_thread_done():", self.recorder.is_recorder_thread_done()
-              )
         if self.recorder.is_recorder_thread_done() == True:
             print("update 2")
             return task.done
@@ -164,8 +148,6 @@ class StreamFramesFromRecorder:
         elif (self.recorder.is_recorder_thread_done() is None):
             print("update 3")
             return task.cont
-
-        # print("awefawef")
 
     def render_last_frame(self):
         """ 0 < a < 1 """
@@ -194,6 +176,82 @@ class StreamFramesFromRecorder:
             x = x[:int(len(x) * self.factor)]
 
             y /= (1e11 * 0.5)
+
+            # x = np.linspace(0., 1., num=len(y))
+
+            self.f2d.clear_plot()
+            self.f2d.plot(x, y)
+
+    def say_finished(self):
+        print("finished!")
+
+
+class StreamFramesFromRecorderTimeDomain:
+    """ stream the frames from the recorder to a Frame2d """
+    def __init__(self, f2d):
+        self.f2d = f2d
+        self.recorder = Recorder_for_live_updating()
+        self.recorder.do_record(
+            lambda: True,
+            lambda: False,
+            lambda: False)
+
+        # self.factor = 1.0e-1 * 0.2
+
+        max_ampl = 1.0
+        self.f2d.set_ylim(-max_ampl, max_ampl)
+        self.f2d.set_xlim(0., 1. # 22028. * self.factor
+        )
+
+        taskMgr.add(self.update_task, 'foo2')
+
+    def update_task(self, task):
+        if self.recorder.is_recorder_thread_done() == True:
+            print("update 2")
+            return task.done
+        elif (self.recorder.is_recorder_thread_done() == False):
+            print("update 1")
+            self.render_last_frame()
+            return task.cont
+        elif (self.recorder.is_recorder_thread_done() is None):
+            print("update 3")
+            return task.cont
+
+    def render_last_frame(self):
+        """ 0 < a < 1 """
+        y = self.recorder.grab_last_frames(num=1)
+
+        if y is not None:
+            y = np.fromstring(np.ravel(y), dtype=np.int32)
+
+            sample_down_indices = np.linspace(0, len(y), num=100).astype(int)[:-1]
+
+
+            y = y[sample_down_indices]
+
+            y = y * 1.5e-9 * 5
+
+            # -------
+            # y = np.fft.fft(y)
+            # y = y[:int(len(y)/4)]
+            # -------
+            # -------
+            # y = np.abs(scipy.fft(y))
+            # freqs = fftpack.fftfreq(len(y), (1./Recorder_for_live_updating.RATE))
+            # print(min(freqs), max(freqs))
+            # -------
+
+            # print("y: ", np.shape(y))
+
+            # normalize y:
+
+            t = np.linspace(0., 1., num=len(y)) # ?
+            x = t
+
+            # y = y[:int(len(y) * self.factor)]
+            # x = x[:int(len(x) * self.factor)]
+
+            # y /= (1e11 * 0.5)
 
             # x = np.linspace(0., 1., num=len(y))
 
@@ -243,20 +301,20 @@ class MyApp(ShowBase):
 
         # ----------- BEGIN FRAME2d experiments --------
 
-        f2d = Frame2d(cg, update_labels_orientation=False, with_ticks=True)
-        f2d.attach_to_render()
+        # f2d = Frame2d(cg, update_labels_orientation=False, with_ticks=True)
+        # f2d.attach_to_render()
 
-        f2d.set_figsize(*np.array([1., 0.5])*1)
+        # f2d.set_figsize(*np.array([1., 0.5])*1)
 
-        f2d.set_xlim(0., 1.)
-        f2d.set_ylim(-1., 1.)
+        # f2d.set_xlim(0., 1.)
+        # f2d.set_ylim(-1., 1.)
 
-        x = np.linspace(0, 1, num=100)
-        f2d.plot(x, np.sin(10*x), color="orange")
-        f2d.plot(x, 2.*np.sin(5*x), color="blue")
-        f2d.plot(x, np.array([2.5]*len(x)), color="white")
+        # x = np.linspace(0, 1, num=100)
+        # f2d.plot(x, np.sin(10*x), color="orange")
+        # f2d.plot(x, 2.*np.sin(5*x), color="blue")
+        # f2d.plot(x, np.array([2.5]*len(x)), color="white")
 
-        f2d.update_alignment()
+        # f2d.update_alignment()
 
         # # ----------- END FRAME2d experiments --------
 
@@ -298,7 +356,30 @@ class MyApp(ShowBase):
         # ---------------------------
 
         # --------------
-        sffr = StreamFramesFromRecorder(f2d)
+
+
+        f2d3 = Frame2d(cg, update_labels_orientation=False, with_ticks=True)
+        f2d3.attach_to_render()
+
+        f2d3.set_figsize(*np.array([1., 0.5])*1)
+        f2d3.setPos(-1.25, 0., -0.125 + -0.5 + 0.5 + 0.25)
+
+        f2d3.update_alignment()
+
+        sffr = StreamFramesFromRecorder(f2d3)
+
+
+        # ---- second frame
+
+        f2d2 = Frame2d(cg, update_labels_orientation=False, with_ticks=True)
+        f2d2.attach_to_render()
+
+        f2d2.set_figsize(*np.array([1., 0.5]))
+        f2d2.setPos(-1.25, 0., -0.125 -0.5)
+
+        f2d2.update_alignment()
+
+        sffr2 = StreamFramesFromRecorderTimeDomain(f2d2)
 
         # sffr.render_last_frame,
 
